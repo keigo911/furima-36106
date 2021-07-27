3fcd54743067e215dbdd8b1eab2031aad39abb52
keigo911 <kig.911jj@gmail.com>
2021-07-27 22:17:07 +0900

    ユーザーモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,11 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :buys
-         
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
   validates :birthday, presence: true
-
 end
