b0e259ad31c2205b06582d7f162ef7cbb2ee9c82
keigo911 <kig.911jj@gmail.com>
2021-07-29 18:26:09 +0900

    activehashの導入、実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,25 @@ class Item < ApplicationRecord
   has_one :buy
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee_burden
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
+
   with_options presence: true do
     validates :products
     validates :products_description
-    validates :category_id
-    validates :status_id
-    validates :fee_burden_id
-    validates :prefecture_id
-    validates :delivery_time_id
-    validates :price
+    validates :price, numericality: { in: 300..9999999 }, format: { with: VALID_PRICE_REGEX }
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+      validates :category_id
+      validates :status_id
+      validates :fee_burden_id
+      validates :prefecture_id
+      validates :delivery_time_id
+    end
   end
-
-
-
 end
