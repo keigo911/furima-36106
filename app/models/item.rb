47d3d867ab960d3ba57dfaa770b04b771d8c2bef
keigo911 <kig.911jj@gmail.com>
2021-08-03 15:36:51 +0900

    販売価格計算、表示の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,10 +13,11 @@ class Item < ApplicationRecord
   VALID_PRICE_REGEX = /\A[0-9]+\z/
 
   with_options presence: true do
+    validates :image
     validates :products
     validates :products_description
-    validates :price, numericality: { in: 300..9999999 }, format: { with: VALID_PRICE_REGEX }
-    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: VALID_PRICE_REGEX }
+    with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :status_id
       validates :fee_burden_id
